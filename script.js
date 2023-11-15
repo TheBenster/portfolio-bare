b681636017daa3ceadc7fbcbc4490f25a32c4882
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-11-14 20:14:58 -0500

    you can see commit history on TOP-portfolio, omitted webpack due to css errors.

---
+++ script.js
@@ -0,0 +1,26 @@
+const navLinks = document.querySelectorAll(".link-to");
+
+function highlightNavLink() {
+  const scrollPosition = window.scrollY;
+  console.log("test");
+
+  navLinks.forEach((link) => {
+    const targetId = link.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      const sectionTop = targetSection.offsetTop;
+      const sectionBottom = sectionTop + targetSection.clientHeight;
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    }
+  });
+}
+
+window.addEventListener("scroll", highlightNavLink);
+
+highlightNavLink();
